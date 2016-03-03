395eac374e8c09b76d1c41a4851b750addf09fd5
Scott Christensen <scottmc526@gmail.com>
2016-03-03 11:29:32 -0700

    newPirate form

--- 01-angular-express-postgres/pirates/public/javascripts/controllers/piratesController.js
+++ 01-angular-express-postgres/pirates/public/javascripts/controllers/piratesController.js
@@ -1,6 +1,13 @@
 app.controller('piratesController', ['$scope', 'piratesService', function($scope, piratesService){
-  piratesService.all().then(function(pirates) {
+    $scope.showForm = false;
+    piratesService.all().then(function(pirates) {
       $scope.pirates = pirates;
       console.log($scope.pirates.pirates);
     })
+
+    $scope.newP = function(){
+      $scope.showForm = !$scope.showForm
+      console.log($scope.showForm);
+    }
+
 }])
