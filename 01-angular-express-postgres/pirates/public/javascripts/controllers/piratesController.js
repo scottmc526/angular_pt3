49725272b92dcf1b9bdbc3a373d1437aa7678c21
Scott Christensen <scottmc526@gmail.com>
2016-03-03 13:08:10 -0700

    new idead

--- 01-angular-express-postgres/pirates/public/javascripts/controllers/piratesController.js
+++ 01-angular-express-postgres/pirates/public/javascripts/controllers/piratesController.js
@@ -10,4 +10,16 @@ app.controller('piratesController', ['$scope', 'piratesService', function($scope
       console.log($scope.showForm);
     }
 
+    $scope.sayHi = function(){
+      console.log('hi');
+    }
+    piratesService.create().then(function(pirates){
+      var pirate = {}
+      pirate.name = $scope.name;
+      pirate.poison = $scope.poison;
+      pirate.accessory = $scope.accessory;
+      console.log(pirate);
+    })
+
+
 }])
--- 01-angular-express-postgres/pirates/public/javascripts/services/pirates_service.js
+++ 01-angular-express-postgres/pirates/public/javascripts/services/pirates_service.js
@@ -4,6 +4,19 @@ app.factory('piratesService', function ($http) {
       return $http.get('/api/pirates').then(function (response) {
         return response.data;
       })
+    },
+
+    create: function(name, poison, accessory){
+      var pirate = {}
+      pirate.name = name;
+      pirate.poison = poison;
+      pirate.accessory = accessory
+      return $http.post('/api/pirates', pirate);
+      console.log(pirate);
+    },
+
+    test: function(){
+      console.log('hitting');
     }
   }
 })
